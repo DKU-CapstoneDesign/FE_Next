5b8548b29e160b07ab58a24846b672ce02c39fb5
ChunEunyu <xhoneyx01@naver.com>
2024-05-15 20:40:01 +0900

    style: 게시글 페이지 스타일링 수정 및 인풋 박스와 좋아요 기능 추가.

--- koreigner/src/app/(afterlogin)/board/post/page.tsx
+++ koreigner/src/app/(afterlogin)/board/post/page.tsx
@@ -1,10 +1,21 @@
+'use client';
 import styles from './page.module.css';
 import NavMenu from "../../_component/NavMenu/NavMenu"
 import Comment from '../_component/Comment/Comment';
 import { IoPersonCircleOutline } from "react-icons/io5";
 import { FaRegCommentDots } from "react-icons/fa";
+import { IoIosHeartEmpty } from "react-icons/io";
+import { IoIosHeart } from "react-icons/io";
+import ChatInput from '../../_component/ChatInput/ChatInput';
+import { useState } from 'react';
 
 export default function PostPage() {
+    const [like, setLike] = useState<boolean>(false);
+
+    function handleClick() {
+        setLike(!like);
+    }
+
     return (
         <>
             <NavMenu isBarVisible={false} />
@@ -35,15 +46,32 @@ export default function PostPage() {
                 </div>
                 <div className={styles.comment}>
                     <div className={styles.commentHeader}>
-                        <FaRegCommentDots className={styles.commentIcon} />
-                        <p>comments</p>
-                        <p className={styles.commentCount}>0</p>
+                        <div className={styles.commentHeaderItem}>
+                            <FaRegCommentDots className={styles.icon} />
+                            <p>comments</p>
+                            <p className={styles.count}>0</p>
+                        </div>
+                        <div className={styles.commentHeaderItem}>
+                            {like && 
+                                <IoIosHeartEmpty 
+                                    onClick={() => handleClick()}
+                                    className={styles.icon} />
+                            }
+                            {!like && 
+                                <IoIosHeart 
+                                    onClick={() => handleClick()}
+                                    className={styles.icon} />
+                            }
+                            <p>like</p>
+                            <p className={styles.count}>0</p>
+                        </div>
                     </div>
                     <div>
                         <Comment />
                         <Comment />
                         <Comment />
                     </div>
+                    <ChatInput />
                 </div>
             </div>
         </>
